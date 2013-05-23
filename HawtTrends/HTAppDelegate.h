c6f966c495143e26ca2782c655669e90726c0528
Pierre Felgines <pierre.felgines@gmail.com>
2013-05-23 22:28:10 +0200

    Add new files

---
+++ HawtTrends/HTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HTAppDelegate.h
+//  HawtTrends
+//
+//  Created by Pierre Felgines on 23/05/13.
+//  Copyright (c) 2013 Pierre Felgines. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HawtTrends/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HawtTrends
+//
+//  Created by Pierre Felgines on 23/05/13.
+//  Copyright (c) 2013 Pierre Felgines. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HTAppDelegate class]));
+    }
+}
