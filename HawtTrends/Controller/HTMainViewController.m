5956ddff668bd1eeb780e7090daca15e5b7e60ac
Pierre Felgines <pierre.felgines@gmail.com>
2014-11-29 14:43:24 +0100

    Create collection view

--- HawtTrends/Controller/HTMainViewController.m
+++ HawtTrends/Controller/HTMainViewController.m
@@ -11,11 +11,12 @@
 #import "HTGridSizeSelector.h"
 #import "HTCountryTableViewController.h"
 
-@interface HTMainViewController () <HTGridSizeSelectorDelegate, HTCountryTableViewControllerDelegate, UIScrollViewDelegate>
+@interface HTMainViewController () <HTGridSizeSelectorDelegate, HTCountryTableViewControllerDelegate, UIScrollViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate>
 @property (nonatomic, strong) UIScrollView * scrollView;
 @property (nonatomic, strong) UIView * contentView;
 @property (nonatomic, strong) HTGridSizeSelector * gridSelector;
 @property (nonatomic, strong) HTCountryTableViewController * countryTableViewController;
+@property (nonatomic, strong) UICollectionView * collectionView;
 @property (nonatomic) HTConfiguration currentConfiguration;
 @end
 
@@ -31,46 +32,67 @@
 - (void)loadView {
     [super loadView];
 
-    _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    _scrollView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [self.view addSubview:_scrollView];
-    _scrollView.pagingEnabled = YES;
-    _scrollView.bounces = NO;
-    _scrollView.showsHorizontalScrollIndicator = NO;
-    _scrollView.backgroundColor = [UIColor whiteColor];
-    _scrollView.delegate = self;
+    [self _createScrollView];
+    [self _createCountryTableView];
+    [self _createCollectionView];
+    [self _createScrollViewContentSizeConstraints];
 
-    _contentView = [[UIView alloc] initWithFrame:self.view.frame];
-    [_scrollView addSubview:_contentView];
-
+//    _contentView = [[UIView alloc] initWithFrame:self.view.frame];
+//    [_scrollView addSubview:_contentView];
+//
     _gridSelector = [[HTGridSizeSelector alloc] initWithFrame:CGRectMake(5.0f, 5.0f, 20.0f, 20.0f)];
     _gridSelector.delegate = self;
     [self.view addSubview:_gridSelector];
 
-    _countryTableViewController = [[HTCountryTableViewController alloc] init];
-    _countryTableViewController.delegate = self;
-    [self addChildViewController:_countryTableViewController];
-    [self.scrollView addSubview:_countryTableViewController.view];
-
     _currentConfiguration = HTConfigurationMake(2, 2);
 }
 
-- (void)viewWillLayoutSubviews {
-    [super viewWillLayoutSubviews];
-    _scrollView.contentSize = CGSizeMake(self.view.bounds.size.width * 2.0f, self.view.bounds.size.height);
-    _contentView.frame = self.view.bounds;
-    _countryTableViewController.view.frame = CGRectMake(self.view.bounds.size.width, 0, self.view.bounds.size.width, self.view.bounds.size.height);
-
-    [self _loadInterfaceWithConfiguration:_currentConfiguration];
-}
-
-- (void)viewDidLayoutSubviews {
-    [super viewDidLayoutSubviews];
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    [super willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    [_collectionView.collectionViewLayout invalidateLayout];
 
     if (self.scrollView.contentOffset.x > 0) {
         self.scrollView.contentOffset = CGPointMake(self.scrollView.bounds.size.width, 0);
     }
 }
+
+#pragma mark - UICollectionViewDataSource methods
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return _currentConfiguration.row * _currentConfiguration.column;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"identifier" forIndexPath:indexPath];
+
+    cell.backgroundColor = [UIColor yellowColor];
+
+    cell.layer.borderColor = [UIColor blackColor].CGColor;
+    cell.layer.borderWidth = 1;
+
+    return cell;
+}
+
+#pragma mark - UICollectionViewFlowLayoutDelegate methods
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    CGSize screenSize = self.view.bounds.size;
+    return CGSizeMake(screenSize.width / _currentConfiguration.row, screenSize.height / _currentConfiguration.column);
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return UIEdgeInsetsZero;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return 0;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 0;
+}
+
 
 #pragma mark - UIScrollViewDelegate methods
 
@@ -104,7 +126,8 @@
 
 - (void)gridSelector:(HTGridSizeSelector *)gridSelector didChoosePosition:(HTPosition)position {
     _currentConfiguration = HTConfigurationMakeFromPosition(position);
-    [self _loadInterfaceWithConfiguration:_currentConfiguration];
+    [_collectionView reloadData];
+//    [self _loadInterfaceWithConfiguration:_currentConfiguration];
 }
 
 #pragma mark - Private methods
@@ -125,6 +148,100 @@
             [_contentView addSubview:cellView];
         }
     }
+}
+
+- (void)_createScrollView {
+    _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    _scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+    _scrollView.pagingEnabled = YES;
+    _scrollView.bounces = NO;
+    _scrollView.showsHorizontalScrollIndicator = NO;
+    _scrollView.backgroundColor = [UIColor whiteColor];
+    _scrollView.delegate = self;
+    [self.view addSubview:_scrollView];
+
+    // Horizontal
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_scrollView]|"
+                                                                      options:0
+                                                                      metrics:0
+                                                                        views:NSDictionaryOfVariableBindings(_scrollView)]];
+
+    // Vertical
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_scrollView]|"
+                                                                      options:0
+                                                                      metrics:0
+                                                                        views:NSDictionaryOfVariableBindings(_scrollView)]];
+}
+
+- (void)_createCountryTableView {
+    _countryTableViewController = [[HTCountryTableViewController alloc] init];
+    _countryTableViewController.delegate = self;
+    [self addChildViewController:_countryTableViewController];
+    [self.scrollView addSubview:_countryTableViewController.view];
+
+    _countryTableViewController.view.translatesAutoresizingMaskIntoConstraints = NO;
+
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_countryTableViewController.view
+                                                          attribute:NSLayoutAttributeHeight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeHeight
+                                                         multiplier:1.0f
+                                                           constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_countryTableViewController.view
+                                                          attribute:NSLayoutAttributeWidth
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeWidth
+                                                         multiplier:1.0f
+                                                           constant:0]];
+
+    UIView * countryTableView = _countryTableViewController.view;
+    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[countryTableView]|"
+                                                                        options:0
+                                                                        metrics:nil
+                                                                          views:NSDictionaryOfVariableBindings(countryTableView)]];
+}
+
+- (void)_createCollectionView {
+    UICollectionViewFlowLayout * flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    _collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:flowLayout];
+    _collectionView.translatesAutoresizingMaskIntoConstraints = NO;
+    _collectionView.backgroundColor = [UIColor redColor];
+    [_scrollView addSubview:_collectionView];
+
+
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_collectionView
+                                                          attribute:NSLayoutAttributeHeight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeHeight
+                                                         multiplier:1.0f
+                                                           constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_collectionView
+                                                          attribute:NSLayoutAttributeWidth
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeWidth
+                                                         multiplier:1.0f
+                                                           constant:0]];
+
+    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_collectionView]|"
+                                                                        options:0
+                                                                        metrics:nil
+                                                                          views:NSDictionaryOfVariableBindings(_collectionView)]];
+
+    [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"identifier"];
+    _collectionView.dataSource = self;
+    _collectionView.delegate = self;
+}
+
+- (void)_createScrollViewContentSizeConstraints {
+    UIView * countryTableView = _countryTableViewController.view;
+    [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_collectionView][countryTableView]|"
+                                                                        options:0
+                                                                        metrics:nil
+                                                                          views:NSDictionaryOfVariableBindings(_collectionView, countryTableView)]];
 }
 
 @end
